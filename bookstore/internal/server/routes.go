b0ccc3f98d7b07662a2cf809a65cb1ca99e315ed
Abhishek Kumar <incredibledatascientist@gmail.com>
2026-03-01 16:47:58 +0530

    Clean the routes and added logging middleware

--- bookstore/internal/server/routes.go
+++ bookstore/internal/server/routes.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"bookstore/internal/handler"
+	"bookstore/internal/middleware"
 	"bookstore/internal/utils"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -15,17 +17,12 @@ var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/books/{id}", handler.GetBook).Methods(http.MethodGet)
 	router.HandleFunc("/books/{id}", handler.UpdateBook).Methods(http.MethodPut)
 	router.HandleFunc("/books/{id}", handler.DeleteBook).Methods(http.MethodDelete)
-	// router.HandleFunc("/books/", handler.CreateBook).Methods(http.MethodPost)
-	// router.HandleFunc("/books/", handler.GetBooks).Methods(http.MethodGet)
-	// router.HandleFunc("/books/{id}/", handler.GetBook).Methods(http.MethodGet)
-	// router.HandleFunc("/books/{id}/", handler.UpdateBook).Methods(http.MethodPut)
-	// router.HandleFunc("/books/{id}/", handler.DeleteBook).Methods(http.MethodDelete)
 }
 
-// func (s *HTTPServer) defaultHandler(w http.ResponseWriter, r *http.Request) {
-// 	err := fmt.Sprintf("The requested endpoint (%v) does not exist", r.RequestURI)
-// 	utils.Error(w, http.StatusNotFound, err, nil)
-// }
+func (s *HTTPServer) defaultHandler(w http.ResponseWriter, r *http.Request) {
+	err := fmt.Sprintf("The requested endpoint (%v) does not exist", r.RequestURI)
+	utils.Error(w, http.StatusNotFound, err, nil)
+}
 
 func (s *HTTPServer) timeHandler(w http.ResponseWriter, r *http.Request) {
 	currentTime := time.Now().Format(time.RFC1123)
@@ -38,22 +35,9 @@ func (s *HTTPServer) healthHandler(w http.ResponseWriter, r *http.Request) {
 	utils.Success(w, http.StatusOK, "Service is healthy", health)
 }
 
-// func (s *HTTPServer) loginHandler(w http.ResponseWriter, r *http.Request) {
-// 	if err := r.ParseForm(); err != nil {
-// 		utils.Error(w, http.StatusBadRequest, "Form parse error", nil)
-// 		return
-// 	}
-
-// 	username := r.FormValue("username")
-// 	password := r.FormValue("password")
-
-// 	resp := map[string]string{"username": username, "password": password}
-// 	utils.Success(w, http.StatusOK, "Post request successful...!", resp)
-// }
-
 func (s *HTTPServer) Routes() http.Handler {
 	r := mux.NewRouter()
-	// r.Use(middleware.Logging)
+	r.Use(middleware.Logging)
 
 	RegisterBookStoreRoutes(r)
 
